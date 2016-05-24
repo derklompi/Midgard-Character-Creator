ac9d41f960fcd27cb2f185fcd83a1a7b33f8f8a3
Fabian Dörr <faflfama@outlook.com>
2016-05-24 16:01:07 +0200

    aa

--- MidgardCreator/Program.cs
+++ MidgardCreator/Program.cs
@@ -17,6 +17,7 @@ namespace MidgardCreator
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMain());
+
         }
     }
 }
