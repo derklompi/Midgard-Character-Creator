eff3dd7791630ef213f4015728d31ce14f7f2ab3
Fabian Dörr <faflfama@outlook.com>
2016-10-18 02:22:33 +0200

    level up form bla bla sleep weee

--- MidgardCreator/Program.cs
+++ MidgardCreator/Program.cs
@@ -33,7 +33,8 @@ namespace mcreator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmMain());
+            //Application.Run(new FrmMain());
+            Application.Run(new FrmLevelUp());
 
         }
     }
